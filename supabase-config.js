445383e0d23440976a2b72572b5023f764b2104a
mkhoiruddinmurtadlo-cyber <mkhoiruddinmurtadlo@gmail.com>
2026-08-28 14:53:31 +0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,3 @@
 // Isi 2 nilai ini dari Supabase Project Settings > API
-window.NUSA_SUPABASE_URL = 'PASTE_SUPABASE_PROJECT_URL';
-window.NUSA_SUPABASE_ANON_KEY = 'PASTE_SUPABASE_ANON_KEY';
+window.NUSA_SUPABASE_URL = 'https://ihkgqlvthgffjlozttts.supabase.co';
+window.NUSA_SUPABASE_ANON_KEY = 'sb_publishable_iNicdVgBB0A56y_iU_huYA_MwwEuVDD';
